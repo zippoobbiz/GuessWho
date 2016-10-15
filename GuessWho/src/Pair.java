dc72a186d0226e89a55c255f24811b953277ee40
Xiaoduo Xu <zippoobbiz@gmail.com>
2016-10-15 13:00:28 +1100

    Add a new attribute
    add chanceToWin attribute
    calculate the percentage of division

--- GuessWho/src/Pair.java
+++ GuessWho/src/Pair.java
@@ -1,8 +1,15 @@
 
-public class Pair {
+public class Pair implements Comparable<Pair> {
 
+	/*
+	 * Pair is an object that store a pair of value and name of the attribute
+	 * chanceToWin is the percentage of the division, closer to 50% is better
+	 * compareTo method compare two Pairs, the winner has the closer chanceToWin to 50%
+	 */
+	
 	private String name;
 	private String value;
+	private double chanceToWin;
 
 	public Pair(String name, String value) {
 		this.name = name;
@@ -23,6 +30,26 @@ public class Pair {
 
 	public void setValue(String value) {
 		this.value = value;
+	}
+
+	public double getChanceToWin() {
+		return chanceToWin;
+	}
+
+	public void setChanceToWin(double chanceToWin) {
+		this.chanceToWin = chanceToWin;
+	}
+
+	@Override
+	public int compareTo(Pair o) {
+		// TODO Auto-generated method stub
+		return Math.abs(this.chanceToWin - 0.5) - Math.abs(o.chanceToWin - 0.5) > 0 ? 1 : -1;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return name + "\t" + value + "\t" + chanceToWin;
 	}
 
 }
